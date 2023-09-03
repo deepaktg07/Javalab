73254970793952347b373a24dff33ea6f65d6060
deepaktg07 <120008495+deepaktg07@users.noreply.github.com>
2023-09-03 19:09:18 +0530

    Update employeeid.java

--- employeeid.java
+++ employeeid.java
@@ -20,7 +20,7 @@ public class EmployeeCommandPrompt {
 			double[] DA={20000,32000,12000,15000,40000};
 			
 			System.out.println("Enter the employee ID:");
-			int id=Integer.parseInt(args[0]);
+			int id=sc.nextInt();
 			int flag=0;
 			double salary=0;
 			for(int i=0;i<empID.length;i++)
